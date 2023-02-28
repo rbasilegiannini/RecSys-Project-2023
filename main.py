deab25abb89ad5dd53902808f7672b30f4dabad0
SimplyMarious <mariothemaster98@hotmail.it>
2023-02-28 13:02:14 +0100

    Folder structure changes

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Welcome to the best Recommender System. Ever.')
-
-
-if __name__ == '__main__':
-    main()
---
+++ src/main.py
@@ -0,0 +1,10 @@
+import dataset_extractor
+
+
+def main():
+    print('Welcome to the best Recommender System.\nEver.')
+    dataset_extractor = dataset_extractor.DatasetExtractor(3)
+
+
+if __name__ == '__main__':
+    main()
