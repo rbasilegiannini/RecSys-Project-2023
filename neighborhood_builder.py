0a214dd781aa2bda4105c6a63753ef4b03d0a0e4
rbgdotbrush <rbg94@hotmail.it>
2023-03-02 22:54:43 +0100

    Add generate_bipartite_network function

--- neighborhood_builder.py
+++ neighborhood_builder.py
@@ -1,34 +1,36 @@
-import random
 import networkx as nx
 import numpy as np
-import matplotlib.pyplot as plt
 
-def generate_network(n):
-    '''
-    :param n:
-        The number of net's nodes
+
+def generate_bipartite_network(urm):
+    """
+    :param urm:
+        The User-Rating Matrix
     :return:
-        A networkX graph
-    '''
+        A bipartite graph
+    """
+    _B = nx.Graph()
 
-    # initialize dictionary with nodes
-    _graph_dct = {node: [] for node in range(n)}    # (node, edges[])
-    _nodes = list(range(n))     # [1, ..., n]
+    # initialize interaction dct with user nodes
+    _interaction_dct = {}
+    for _user in range(urm.shape[0]):
+        _interaction_dct[_user] = []
+        _B.add_node(_user, bipartite=0)  # Users set
 
-    # generate edges. For each (node, edges[]), generate a random collection of edges
-    for _n, _edge_list in _graph_dct.items():
-        _edge_c = random.randint(min(_nodes), int(max(_nodes) / 2)) # edges' cardinality
-        _el = random.sample(_nodes, _edge_c)
-        _graph_dct[_n] = _el
+    # An offset to distinguish users from items id
+    offset = urm.shape[0]
 
-    # create networkx multi-edge graph
-    _G = nx.MultiGraph(_graph_dct)
-    return _G
+    for _item in range(urm.shape[1]):
+        _B.add_node(_item + offset, bipartite=1)  # Items set
 
-n = 20
-G = generate_network(n)
+    # Retrieve user-items interaction
+    for _user, _items in _interaction_dct.items():
+        _interaction_dct[_user] = np.array(np.where(urm[_user] == 1)).flatten()
+        _interaction_dct[_user] += offset
 
-# visualize graph
-pos = nx.spring_layout(G)
-nx.draw(G, pos, node_size = 75, alpha = 0.8)
-plt.show()
+        # Add edges only between nodes of opposite node sets
+        for _item in _interaction_dct[_user]:
+            _B.add_edge(_user, _item)
+
+    if nx.is_bipartite(_B):
+        return _B
