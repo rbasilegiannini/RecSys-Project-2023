b8c65b6812ee493d14c1287eda641cc97180076b
rbgdotbrush <rbg94@hotmail.it>
2023-03-06 21:17:33 +0100

    Add binary_encoding

---
+++ src/encoder.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def binary_encoding(dim, index_elements):
+    """
+    :param dim:
+        The array's length
+    :param index_elements:
+        Indexes to be set to 1
+    :return:
+        The binary vector
+    """
+    binary_vector = np.zeros(dim, int)
+    for idx in index_elements:
+        binary_vector[idx] = 1
+
+    return binary_vector
