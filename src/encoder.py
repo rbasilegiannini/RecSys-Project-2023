273b9f456ad9838ebe60d6aaad0dea39ab8f1c2a
SimplyMarious <mariothemaster98@hotmail.it>
2023-03-08 11:32:22 +0100

    get_one_hot_encoding now using np

--- src/encoder.py
+++ src/encoder.py
@@ -2,8 +2,5 @@ import numpy as np
 
 
 def get_onehot_encoding(size):
-    onehot_matrix = np.zeros((size, size), dtype=int)
-    for i in range(size):
-        onehot_matrix[i][i] = 1
-
+    onehot_matrix = np.identity(size, dtype=int)
     return onehot_matrix
