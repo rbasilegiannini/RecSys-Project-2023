734a31904373c60492457d367b1b4855f53079ea
rbgdotbrush <rbg94@hotmail.it>
2023-03-15 20:20:19 +0100

    Add comments in Embedding class

--- src/embedding_builder.py
+++ src/embedding_builder.py
@@ -2,6 +2,12 @@ import numpy as np
 
 class Embedding:
     def __init__(self, urm, latent_factor_size):
+        """
+        :param urm:
+            The User-Rating Matrix
+        :param latent_factor_size:
+            The number of latent factors
+        """
         [u, s, vh] = np.linalg.svd(urm, full_matrices=False)
         s = np.diag(s)
 
@@ -15,9 +21,17 @@ class Embedding:
         self.__V_T = s @ vh
 
     def get_user_embeddings(self):
+        """
+        :return:
+            Matrix of user latent vectors
+        """
         return self.__U
 
     def get_item_embeddings(self):
+        """
+        :return:
+            Matrix of item latent vectors (transpose)
+        """
         return self.__V_T
 
 
