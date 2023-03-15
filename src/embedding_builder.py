2b9c421293d90f342bd57870982f2e92c25f8819
rbgdotbrush <rbg94@hotmail.it>
2023-03-15 20:04:18 +0100

    Complete Embedding class

--- src/embedding_builder.py
+++ src/embedding_builder.py
@@ -1,12 +1,18 @@
 import numpy as np
 
 class Embedding:
-    def __init__(self, urm):
-        [u, s, vh] = np.linalg.svd(urm)
+    def __init__(self, urm, latent_factor_size):
+        [u, s, vh] = np.linalg.svd(urm, full_matrices=False)
+        s = np.diag(s)
 
-        # compute U and V
+        # Take most significant features
+        u = u[:, 0:latent_factor_size]
+        s = s[0:latent_factor_size, 0: latent_factor_size]
+        vh = vh[0:latent_factor_size, :]
+
+        # Compute U and V
         self.__U = u
-        self.__V_T = np.diag(s) @ vh
+        self.__V_T = s @ vh
 
     def get_user_embeddings(self):
         return self.__U
