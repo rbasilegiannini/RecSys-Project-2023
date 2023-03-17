618c447c9ab467c0bd5ed69c91f76e4174105603
rbgdotbrush <rbg94@hotmail.it>
2023-03-17 16:42:38 +0100

    Refact main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import dataset_extractor as ds_extractor
 import neighborhood_builder as nb_builder
 import embedding_builder as emb_builder
@@ -19,29 +21,18 @@ def main():
     # Retrieve URM and one-hot encoding
     dataset_extractor = ds_extractor.DatasetExtractor(USERS_SIZE, ITEMS_SIZE)
     urm = dataset_extractor.get_urm()
-
     onehot_users = encoder.get_onehot_encoding(USERS_SIZE)
     onehot_items = encoder.get_onehot_encoding(ITEMS_SIZE)
 
     # Retrieve neighborhoods and binary encoding
     [users_neighborhood, items_neighborhood] = nb_builder.extract_neighborhood(urm, hyperparams['resolution'])
-
-    binary_users_neighborhood = []
-    for neighborhood in users_neighborhood:
-        binary_users_neighborhood.append(
-            encoder.get_binary_encoding(ITEMS_SIZE, neighborhood)
-        )
-
-    binary_items_neighborhood = []
-    for neighborhood in items_neighborhood:
-        binary_items_neighborhood.append(
-            encoder.get_binary_encoding(USERS_SIZE, neighborhood)
-        )
+    binary_users_neighborhood = encoder.get_neighborhoods_encoding(users_neighborhood, ITEMS_SIZE)
+    binary_items_neighborhood = encoder.get_neighborhoods_encoding(items_neighborhood, USERS_SIZE)
 
     # Compute latent vectors
     embedding = emb_builder.Embedding(urm, hyperparams['k'])
-    user_latent_vectors = embedding.get_user_embeddings()
-    item_latent_vectors = embedding.get_item_embeddings()
+    users_latent_vector = embedding.get_user_embeddings()
+    items_latent_vector = embedding.get_item_embeddings()
 
     del embedding   # Optimization
 
