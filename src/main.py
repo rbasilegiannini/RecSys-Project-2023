0d241f77c4eba3e6651e642f51c18a606874a08d
rbgdotbrush <rbg94@hotmail.it>
2023-05-22 15:56:25 +0200

    Update main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+import numpy as np
+import time
 import dataset_extractor as ds_extractor
-import MLP_training_set_builder as ts_builder
-# import classification_test
+import MLP_training_set_builder as ds_builder
+import tensorflow as tf
 import NNCF
 
 USERS_SIZE = 943
@@ -8,12 +10,12 @@ ITEMS_SIZE = 1682
 
 
 hyperparams = {
-    "res": 0.5,
+    "res": 0.6,
     "k": 10,
     "hidden layers": 1,
-    "neurons": 5,
+    "neurons": 1,
     "activation": 'sigmoid',
-    "max epochs": 10
+    "max epochs": 350
 }
 
 
@@ -26,6 +28,12 @@ def main():
     dataset_extractor = ds_extractor.DatasetExtractor(USERS_SIZE, ITEMS_SIZE)
     urm = dataset_extractor.get_urm()
     test_items = dataset_extractor.get_test_items()
+
+    # Retrieve the items to avoid in the learning task (they will be used for recommendation)
+    items_to_avoid = np.ndarray([USERS_SIZE, 101], dtype=int)
+    for user in range(USERS_SIZE):
+        items_to_avoid[user] = dataset_extractor.get_not_interacted_items_for_recommendation(user)
+
     print(" Complete.")
 
     # Building and learning NNCF
@@ -37,30 +45,72 @@ def main():
                     hyperparams['activation']
                     )
 
+    run_int_comp_time_start = time.time()
+
     user_item_concatenated_embeddings = net.run_integration_component()
-    training_set = ts_builder.get_training_set(urm, user_item_concatenated_embeddings, test_items)
+
+    run_int_comp_time_end = time.time()
+    run_int_comp_time = run_int_comp_time_end - run_int_comp_time_start
+    print("Integration component time: " + str(round(run_int_comp_time, 2)) + "s")
+
+    training_set = ds_builder.get_training_set(urm, user_item_concatenated_embeddings, items_to_avoid)
     net.learning_MLP(training_set, hyperparams['max epochs'])
 
     # Testing
     top_k = 20
-    evaluate_recsys(dataset_extractor, net, top_k, test_items)
+    not_interacted_items = items_to_avoid
+    return evaluate_recsys(net, top_k, test_items, not_interacted_items)
 
 
-def evaluate_recsys(dataset_extractor, net, top_k, test_items):
-    hit = 0
+def evaluate_recsys(net, top_k, test_items, not_interacted_items):
+    hit20 = 0
+    hit10 = 0
+    hit5 = 0
+
     for user in range(USERS_SIZE):
-        not_interacted_items_for_recommendation = dataset_extractor.get_not_interacted_items_for_recommendation(user)
-        recommendations = net.get_recommendations(user, not_interacted_items_for_recommendation, top_k)
 
-        if test_items[user] in recommendations:
-            hit += 1
-        print("New hit with user " + str(user) + ". Current hits: " + str(hit))
+        recommendations_20 = net.get_recommendations(user, not_interacted_items[user], top_k)
+        recommendations_10 = recommendations_20[:10]
+        recommendations_5 = recommendations_20[:5]
 
-    hrk = round((hit / USERS_SIZE) * 100, 2)
-    print("HR@" + str(top_k) + ": " + str(hrk) + "%")
+        if test_items[user] in recommendations_20:
+            hit20 += 1
 
-    return hrk
+        if test_items[user] in recommendations_10:
+            hit10 += 1
+
+        if test_items[user] in recommendations_5:
+            hit5 += 1
+
+    hr20 = round((hit20 / USERS_SIZE) * 100, 2)
+    hr10 = round((hit10 / USERS_SIZE) * 100, 2)
+    hr5 = round((hit5 / USERS_SIZE) * 100, 2)
+
+    print("HR@" + str(20) + ": " + str(hr20) + "%")
+    print("HR@" + str(10) + ": " + str(hr10) + "%")
+    print("HR@" + str(5) + ": " + str(hr5) + "%")
+
+    return [hr5, hr10, hr20]
 
 
 if __name__ == '__main__':
-    main()
+
+    # tf.config.set_visible_devices([], 'GPU')  # For Apple Silicon
+
+    hr5_list = []
+    hr10_list = []
+    hr20_list = []
+    for attempt in range(2):
+        hr_values = main()
+        hr5_list.append(hr_values[0])
+        hr10_list.append(hr_values[1])
+        hr20_list.append(hr_values[2])
+
+    hr5_values = np.array(hr5_list)
+    hr10_values = np.array(hr10_list)
+    hr20_values = np.array(hr20_list)
+
+    print("HR@20 mean: " + str(hr20_values.mean()) + "%")
+    print("HR@10 mean: " + str(hr10_values.mean()) + "%")
+    print("HR@5 mean: " + str(hr5_values.mean()) + "%")
+
