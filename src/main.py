90371e6643de6f6e7e5ef137bacf33754cd61d08
rbgdotbrush <rbg94@hotmail.it>
2023-05-22 16:27:04 +0200

    Refactoring main

--- src/main.py
+++ src/main.py
@@ -15,26 +15,13 @@ hyperparams = {
     "hidden layers": 1,
     "neurons": 1,
     "activation": 'sigmoid',
-    "max epochs": 350
+    "max epochs": 5
 }
 
 
-def main():
+def build_recsys(urm, items_to_avoid):
     print('Welcome to the best Recommender System.\nEver.')
     print()
-
-    # Retrieve URM and Test items list
-    print("URM extraction...", end="")
-    dataset_extractor = ds_extractor.DatasetExtractor(USERS_SIZE, ITEMS_SIZE)
-    urm = dataset_extractor.get_urm()
-    test_items = dataset_extractor.get_test_items()
-
-    # Retrieve the items to avoid in the learning task (they will be used for recommendation)
-    items_to_avoid = np.ndarray([USERS_SIZE, 101], dtype=int)
-    for user in range(USERS_SIZE):
-        items_to_avoid[user] = dataset_extractor.get_not_interacted_items_for_recommendation(user)
-
-    print(" Complete.")
 
     # Building and learning NNCF
     net = NNCF.NNCF(urm,
@@ -46,9 +33,7 @@ def main():
                     )
 
     run_int_comp_time_start = time.time()
-
     user_item_concatenated_embeddings = net.run_integration_component()
-
     run_int_comp_time_end = time.time()
     run_int_comp_time = run_int_comp_time_end - run_int_comp_time_start
     print("Integration component time: " + str(round(run_int_comp_time, 2)) + "s")
@@ -56,10 +41,7 @@ def main():
     training_set = ds_builder.get_training_set(urm, user_item_concatenated_embeddings, items_to_avoid)
     net.learning_MLP(training_set, hyperparams['max epochs'])
 
-    # Testing
-    top_k = 20
-    not_interacted_items = items_to_avoid
-    return evaluate_recsys(net, top_k, test_items, not_interacted_items)
+    return net
 
 
 def evaluate_recsys(net, top_k, test_items, not_interacted_items):
@@ -93,15 +75,29 @@ def evaluate_recsys(net, top_k, test_items, not_interacted_items):
     return [hr5, hr10, hr20]
 
 
-if __name__ == '__main__':
-
-    # tf.config.set_visible_devices([], 'GPU')  # For Apple Silicon
-
+def main():
     hr5_list = []
     hr10_list = []
     hr20_list = []
+
+    top_k = 20
+
+    # Retrieve URM and Test items list
+    print("URM extraction...", end="")
+    dataset_extractor = ds_extractor.DatasetExtractor(USERS_SIZE, ITEMS_SIZE)
+    urm = dataset_extractor.get_urm()
+    test_items = dataset_extractor.get_test_items()
+
+    # Retrieve the items to avoid in the learning task (they will be used for recommendation)
+    not_interacted_items = np.ndarray([USERS_SIZE, 101], dtype=int)
+    for user in range(USERS_SIZE):
+        not_interacted_items[user] = dataset_extractor.get_not_interacted_items_for_recommendation(user)
+
+    print(" Complete.")
+
     for attempt in range(2):
-        hr_values = main()
+        recsys = build_recsys(urm, not_interacted_items)
+        hr_values = evaluate_recsys(recsys, top_k, test_items, not_interacted_items)
         hr5_list.append(hr_values[0])
         hr10_list.append(hr_values[1])
         hr20_list.append(hr_values[2])
@@ -113,4 +109,9 @@ if __name__ == '__main__':
     print("HR@20 mean: " + str(hr20_values.mean()) + "%")
     print("HR@10 mean: " + str(hr10_values.mean()) + "%")
     print("HR@5 mean: " + str(hr5_values.mean()) + "%")
+
+
+if __name__ == '__main__':
+    # tf.config.set_visible_devices([], 'GPU')  # For Apple Silicon
+    main()
 
