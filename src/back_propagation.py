d4eeedaa9f3161bf6464b0b0826c0dd9afb87bcb
rbgdotbrush <rbg94@hotmail.it>
2023-05-26 14:00:48 +0200

    Refactoring __back_propagation method

--- src/back_propagation.py
+++ src/back_propagation.py
@@ -10,6 +10,7 @@ class BackPropagation:
 
     def __init__(self, NN):
 
+        self.__all_activation = None
         self.__NN = NN
         self.__num_layers = NN.get_num_layer()
         self.__a_func_type = NN.get_activation_function()
@@ -40,11 +41,11 @@ class BackPropagation:
          """
 
         # Forward propagation step
-        all_activation, all_output = self.__NN.compute_network(input_data)
+        self.__all_activation, all_output = self.__NN.compute_network(input_data)
         output_lines = all_output[-1]
 
         # Back propagation step
-        self.__back_propagation(targets, all_activation, output_lines)
+        self.__back_propagation(targets, output_lines)
 
         # Compute the error gradient
         gradE = np.zeros(self._num_of_params)
@@ -76,49 +77,77 @@ class BackPropagation:
 
         return gradE
 
-    def __back_propagation(self, targets, all_activation, output_lines):
+    def __back_propagation(self, targets, output_lines):
         """
         This method is used to compute the deltas.
 
         :param output_lines:
             The NN's output lines.
-        :param all_activation:
-            The activation values for each layer.
         :param targets:
              The targets used to compute the error gradient (a numpy array).
         :return:
         """
-        last_activation = all_activation[-1]
 
         # Compute delta output
         delta_out = np.zeros(self.__num_of_output_neurons)
         for line in range(self.__num_of_output_neurons):
-            a_k = last_activation[line]
-            y_k = output_lines[line]
-            t_k = targets[line]
-
-            delta_out_k = af.activation_function_der[self.__output_func_type](a_k) * ef.cross_entropy_derivative(y_k, t_k)
-            delta_out[line] = delta_out_k
+            delta_out[line] = self.__compute_output_delta(output_lines, targets, line)
         self.__deltas_per_layer[-1] = delta_out
 
         # Compute internal deltas
         num_hidden_layer = self.__num_layers - 1
         for layer in reversed(range(num_hidden_layer)):
             num_neurons_layer = self.__all_neurons[layer]
-            activation_layer = all_activation[layer]
-            next_weights_layer = self.__all_weights[layer + 1]
-            next_delta_row = self.__deltas_per_layer[layer + 1]
 
-            delta_i = np.zeros(num_neurons_layer)
+            deltas_layer = np.zeros(num_neurons_layer)
             for neuron in range(num_neurons_layer):
-                # delta_i = compute_neuron_delta(neuron, )
+                deltas_layer[neuron] = self.__compute_internal_delta(layer, neuron)
+            self.__deltas_per_layer[layer] = deltas_layer
 
-                a_i = activation_layer[neuron]
-                next_weights_column = next_weights_layer[:, neuron]
+    def __compute_internal_delta(self, layer, neuron):
+        """
+        This method is used to compute the internal delta.
 
-                summation = next_delta_row @ next_weights_column
-                delta_i_k = af.activation_function_der[self.__a_func_type](a_i) * summation
-                delta_i[neuron] = delta_i_k
+        :param layer:
+            The neuron's layer.
+        :param neuron:
+            The neuron whose delta we are calculating.
+        :return:
+            The neuron's delta.
+        """
+        activation_layer = self.__all_activation[layer]
+        next_weights_layer = self.__all_weights[layer + 1]
+        next_delta_row = self.__deltas_per_layer[layer + 1]
 
-            self.__deltas_per_layer[layer] = delta_i
+        a_i = activation_layer[neuron]
+        next_weights_column = next_weights_layer[:, neuron]
+
+        summation = next_delta_row @ next_weights_column
+        delta_i_k = af.activation_function_der[self.__a_func_type](a_i) * summation
+        delta_i = delta_i_k
+
+        return delta_i
+
+    def __compute_output_delta(self, output_lines, targets, line):
+        """
+        This method is used to compute the output delta.
+
+        :param output_lines:
+            The NN's output lines.
+        :param targets:
+            The targets involved in the computation.
+        :param line:
+            The line of interest.
+        :return:
+        """
+        last_activation = self.__all_activation[-1]
+
+        a_k = last_activation[line]
+        y_k = output_lines[line]
+        t_k = targets[line]
+
+        delta_out_k = af.activation_function_der[self.__output_func_type](a_k) * ef.cross_entropy_derivative(y_k, t_k)
+
+        return delta_out_k
+
 
